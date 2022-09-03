66e158c797b2f8962b0f2118ed19550824083208
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-09-03 19:21:23 +0400

    Refactoring the code and adding context api

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./styles/general/Reset.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { FormProvider } from "./context/FormContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FormProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FormProvider>
   </React.StrictMode>
 );
